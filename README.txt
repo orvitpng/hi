6f6e50e578334acca768466a3774b59e7a2f4ba0
Carter Davis <carterd1016@gmail.com>
2025-12-18 00:59:02 +0000

    aarch64 v1

--- README.txt
+++ README.txt
@@ -1,7 +1,15 @@
 hi
 ==
 
-Have you ever wanted to build the smallet Linux Hello, world! possible*? Well...
-Here you are.
+Have you ever wanted to build the smallet "Hello, world!" possible*? Well..
+here you are!
 
-*=it might not actually be the smallest possible, but I promise it is teeny
+*=it might not actually be the smallest possible, but it would be close**
+**=im not sure how close it is, its the best I could do at least
+
+---------------------------------------------------------------------------
+
+Be wary of using `qemu-user` to emulate these. QEMU (and likely any other form
+of user-mode emulation) uses its own loaders which, while matching
+specifications, do not allow for many of the hacks used in this project to
+optimize for size. Your best bet is full operating system emulation.
--- build.zig
+++ build.zig
@@ -1,30 +1,29 @@
 const builtin = @import("builtin");
 const std = @import("std");
 
+const arches: []const Arch = &.{
+    .{ "aarch64.S", .aarch64 },
+};
+
 pub fn build(b: *std.Build) void {
-    const arch = b.option(
-        std.Target.Cpu.Arch,
-        "target",
-        "The target arch",
-    ) orelse builtin.cpu.arch;
-
-    const target = b.resolveTargetQuery(.{ .cpu_arch = arch });
-    const mod = b.createModule(.{ .target = target });
-
     const src = b.path("src");
-    mod.addAssemblyFile(
-        src.path(
-            b,
-            switch (arch) {
-                .aarch64 => "aarch64.S",
-                else => @panic("unsupported target"),
-            },
-        ),
-    );
+    for (arches) |arch| {
+        const target = b.resolveTargetQuery(.{ .cpu_arch = arch.@"1" });
+        const mod = b.createModule(.{ .target = target });
+        mod.addAssemblyFile(src.path(b, arch.@"0"));
 
-    const obj = b.addObject(.{ .name = "hi", .root_module = mod });
-    const copy = b.addObjCopy(obj.getEmittedBin(), .{ .format = .bin });
+        const obj = b.addObject(.{
+            .name = @tagName(arch.@"1"),
+            .root_module = mod,
+        });
+        const copy = b.addObjCopy(obj.getEmittedBin(), .{ .format = .bin });
 
-    const install = b.addInstallBinFile(copy.getOutput(), obj.name);
-    b.getInstallStep().dependOn(&install.step);
+        const install = b.addInstallBinFile(copy.getOutput(), obj.name);
+        b.getInstallStep().dependOn(&install.step);
+    }
 }
+
+const Arch = struct {
+    []const u8,
+    std.Target.Cpu.Arch,
+};
